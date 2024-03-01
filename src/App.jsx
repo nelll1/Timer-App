e244edc8c86e968312b6481dc133e029346e60e9
nelll1 <92243663+nelll1@users.noreply.github.com>
2024-03-01 11:34:38 +0800

    Time Container

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './Components/style/index.css'
+import Timer  from "./Components/Timer";
+function App(){
+    return <div className="container">
+        <Timer/>
+    </div>
+}
+
+export default App;
---
+++ src/Components/Timer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './style/timer.css'
+function Timer(){
+    return <div className="timerContainer">
+        <div className="secondContainer">
+            <div className="timeContainer">
+                
+            </div>
+        </div>
+    </div>
+}
+
+export default Timer;
